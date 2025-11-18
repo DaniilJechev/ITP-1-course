1e29141fb6ed1913642ba78e2d1bca83a942b319
DaniilJechev <daniilzhechev.bfg@gmail.com>
2025-11-18 16:54:58 +0300

    renamed file

--- src/Lab11_Ex1.java
+++ src/Lab11_Ex1.java
@@ -1,11 +1,11 @@
 import java.io.*;
 import java.nio.file.Paths;
 
-public class Lab11 {
+public class Lab11_Ex1 {
     public static void main() {
         System.out.println(Paths.get("").toAbsolutePath());
         try( FileInputStream in = new FileInputStream("input.txt");
-            FileOutputStream out = new FileOutputStream("output.txt"))
+             FileOutputStream out = new FileOutputStream("output.txt"))
         {
             byte[] arr = new byte[in.available()];
             in.read(arr, 0, arr.length);
