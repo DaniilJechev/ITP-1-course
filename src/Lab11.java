462c5a7bcd09ca0e60ab3fb5fb2988de8bcd6534
DaniilJechev <daniilzhechev.bfg@gmail.com>
2025-11-18 16:52:20 +0300

    Ex1 of lab11 completed

---
+++ src/Lab11.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.nio.file.Paths;
+
+public class Lab11 {
+    public static void main() {
+        System.out.println(Paths.get("").toAbsolutePath());
+        try( FileInputStream in = new FileInputStream("input.txt");
+            FileOutputStream out = new FileOutputStream("output.txt"))
+        {
+            byte[] arr = new byte[in.available()];
+            in.read(arr, 0, arr.length);
+            out.write(arr);
+        } catch(IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
